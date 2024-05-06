248d0f10b8739f3ac1284681c8d81934117e0bfd
atultvarghese <atultvarghese@gmail.com>
2024-05-06 17:42:17 +0530

    login and reg pages

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,7 +45,15 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.legacy.support.v13)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.room:room-runtime:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
+    kapt("androidx.room:room-compiler:2.2.5")
 }
---
+++ automotive/src/main/java/com/atultvarghese/carcare/Registration.kt
@@ -0,0 +1,88 @@
+package com.atultvarghese.carcare
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import com.example.blinkit.dbconnection.UsersDatabase
+import com.example.blinkit.dbconnection.UsersEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import kotlin.concurrent.thread
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registeration)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var usernameText = findViewById<EditText>(R.id.regi_user)
+        var passwordText = findViewById<EditText>(R.id.regi_pass)
+        var registerButton = findViewById<Button>(R.id.regi_button)
+
+        var db = Room.databaseBuilder(this, UsersDatabase::class.java, "users")
+            .fallbackToDestructiveMigration().build()
+
+        var h = Handler()
+
+
+        registerButton.setOnClickListener(){
+
+            GlobalScope.launch {
+                var users = UsersEntity()
+                users.user_name = usernameText.text.toString()
+                users.password = passwordText.text.toString()
+                try {
+                    val newRowId = db.usersDbCreate().addData(users)
+                    if (newRowId.toInt() != -1){
+                        withContext(Dispatchers.Main) {
+                            showToast( "Registered Successfully")
+                            startActivity(Intent(this@Registration, MainActivity::class.java))
+                        }
+                    }else {
+                        withContext(Dispatchers.Main) {
+                            showToast( "User Name Already exists")
+                        }
+                    }
+
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        showToast( "Error Registering $e")
+                    }
+
+                }
+            }
+        }
+
+        registerButton.setOnLongClickListener(){
+            thread {
+                db.usersDbCreate().getAllData().forEach {
+                    h.post {
+                        var result = "User Name : ${it.user_name} with password ${it.password}"
+                        Thread.sleep(1000)
+                        Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    fun showToast(message : String){
+        Toast.makeText(this, message , Toast.LENGTH_LONG).show()
+    }
+}
