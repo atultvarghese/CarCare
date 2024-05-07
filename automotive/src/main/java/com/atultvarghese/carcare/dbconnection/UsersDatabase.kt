5d8555a6b6221fb302081c4437c978d5a35ce1b3
atultvarghese <atultvarghese@gmail.com>
2024-05-07 10:48:15 +0530

    login and registration pages

--- automotive/src/main/java/com/atultvarghese/carcare/dbconnection/UsersDatabase.kt
+++ automotive/src/main/java/com/atultvarghese/carcare/dbconnection/UsersDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [UsersEntity::class], version = 3)
+@Database(entities = [UsersEntity::class], version = 1)
 abstract class UsersDatabase : RoomDatabase() {
     abstract fun usersDbCreate() : UsersDao
 
